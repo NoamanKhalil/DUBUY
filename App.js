2e265b30dd54b182b06af60742959d9df9477c5a
Noaman <caedriel@live.com>
2020-12-09 21:52:15 +0400

    added apptext input bar to take user input and simplified colors and styles

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { Fragment } from 'react';
-import {Dimensions ,SafeAreaView, StyleSheet,TouchableWithoutFeedback,Alert, Text, View ,Image, Button,Platform, BackHandler} from 'react-native';
+import React, { Fragment,useState } from 'react';
+import {Dimensions ,SafeAreaView, StyleSheet,TouchableWithoutFeedback,Alert, Text, View ,Image, Button,Platform, BackHandler, TextInput} from 'react-native';
 import{ useDimensions,useDeviceOrientation } from '@react-native-community/hooks';
 import AccountScreen from './app/screens/AccountScreen';
 import ListingsScreen from './app/screens/ListingsScreen';
+import Screen from "./app/components/Screen";
+import AppTextInput from './app/components/AppTextInput';
 
 
 export default function App() {
+
+const [firstName,setFirstName]=useState('');
+
+
   return (
-      <ListingsScreen/>
+      <Screen>
+      <AppTextInput placeholder="Username" icon="email"/>
+      </Screen>
     );
 }
