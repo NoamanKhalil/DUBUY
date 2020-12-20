cb4b7de787d220b237e4e866ff403635456d0fc9
Noaman <caedriel@live.com>
2020-12-20 23:00:19 +0400

    added scatergory select menus UI

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Fragment,useState } from 'react';
-import {Dimensions ,SafeAreaView, StyleSheet,TouchableWithoutFeedback,Alert, Text, View ,Image, Button,Platform, BackHandler, TextInput} from 'react-native';
+import {Dimensions ,SafeAreaView, StyleSheet,TouchableWithoutFeedback,Alert, Text, View ,Image, Button,Platform, BackHandler, TextInput, Switch} from 'react-native';
 import{ useDimensions,useDeviceOrientation } from '@react-native-community/hooks';
 import AccountScreen from './app/screens/AccountScreen';
 import ListingsScreen from './app/screens/ListingsScreen';
 import Screen from "./app/components/Screen";
 import AppTextInput from './app/components/AppTextInput';
+import AppPicker from './app/components/AppPicker';
 
+
+const categories = [
+  {label:"Furniture",value:1},
+  {label:"Clothing",value:2},
+  {label:"Cameras",value:3},
+];
 
 export default function App() {
 
-const [firstName,setFirstName]=useState('');
+const [category,setCategory]=useState();
 
 
   return (
       <Screen>
-      <AppTextInput placeholder="Username" icon="email"/>
+      <AppPicker selectedItem={category} onSelectedItem={item=>setCategory(item)} items={categories} icon="apps" placeholder="Category"/>
+      <AppTextInput icon="email" placeholder = "Email"/>
       </Screen>
     );
 }
