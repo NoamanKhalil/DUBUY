845591d97c2c3c501044aa0205a90c0faf84760e
Noaman <caedriel@live.com>
2021-01-12 16:43:53 +0400

    n

---
+++ app/components/ImageInputList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+function ImageInputList(props) {
+return (
+<View style={styles.container}></View>
+);
+}
+
+const styles = StyleSheet.create({
+container:
+{
+  flexDirection:'row',
+}
+
+})
+
+export default ImageInputList;
