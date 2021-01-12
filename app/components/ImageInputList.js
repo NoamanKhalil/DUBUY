df97f8ebda8954fc12cfe66eea59790b8574ddc1
Noaman <caedriel@live.com>
2021-01-12 19:29:18 +0400

    added image scroll and multi image support

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,33 +1,41 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import ImageInput from './ImageInput';
+import React, { useRef } from "react";
+import { ScrollView, StyleSheet, View } from "react-native";
+import ImageInput from "./ImageInput";
 
-function ImageInputList({imageUris=[],onRemoveImage,onAddImage}) {
-return (
-<View style={styles.container}>
-  {imageUris.map((uri)=>
-  <View style={styles.image} key={uri}>
-  <ImageInput imageUri={uri}  onChangeImage={()=>onRemoveImage(uri)}/>
-  </View>
-  )}
+function ImageInputList({ imageUris = [], onRemoveImage, onAddImage }) {
+  const ScrolView = useRef();
 
-  <ImageInput onChangeImage={(uri) => onAddImage(uri)}/>
+  return (
+    <View>
+      <ScrollView
+        ref={ScrolView}
+        horizontal
+        onContentSizeChange={() => ScrolView.current.scrollToEnd()}
+      >
+        <View style={styles.container}>
+          {imageUris.map((uri) => (
+            <View style={styles.image} key={uri}>
+              <ImageInput
+                imageUri={uri}
+                onChangeImage={() => onRemoveImage(uri)}
+              />
+            </View>
+          ))}
 
-</View>
-);
+          <ImageInput onChangeImage={(uri) => onAddImage(uri)} />
+        </View>
+      </ScrollView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-container:
-{
-  flexDirection:'row',
-},
-image:
-{
-  marginRight:15,
-}
-
-
-})
+  container: {
+    flexDirection: "row",
+  },
+  image: {
+    marginRight: 15,
+  },
+});
 
 export default ImageInputList;
