87480c80550fcfe8570b7c7c7a59979e562b52e7
Noaman <caedriel@live.com>
2021-01-12 17:47:47 +0400

    added image list

--- App.js
+++ App.js
@@ -12,39 +12,30 @@ import ListingEditScreen from './app/screens/ListingEditScreen';
 import ImageInput from './app/components/ImageInput';
 import * as ImagePicker from "expo-image-picker";
 import * as Permissions from "expo-permissions";
+import ImageInputList from './app/components/ImageInputList';
 
 
 
 export default function App()
 {
-const requestPermission = async () =>
+
+const [imageUris, setImageUris]= useState([]);
+
+const handleAdd= uri=>
 {
-  const {granted} = await ImagePicker.requestCameraRollPermissionsAsync();
-  if (!granted) alert ("You need to enable permission to access the camera roll")
-};
-
-const [imageUri, setImageUri]= useState();
-
-const selectImage = async()=>{
-  try
-  {
-    const result = await ImagePicker.launchImageLibraryAsync();
-    if (!result.cancelled)
-    setImageUri(result.uri)
-
-  }
-  catch(error)
-  {
-    console.log ('Error reading an image',error)
-    
-  }
+  setImageUris([...imageUris,uri])
 }
 
+const handleRemove= uri=>
+{
+  setImageUris(imageUris.filter(imageUri => imageUri !==uri));
+}
   return (
       <Screen>
-      <ImageInput
-       imageUri={imageUri}
-       onChangeImage={(uri)=> setImageUri(uri)}
+      <ImageInputList
+       imageUris={imageUris}
+       onAddImage={handleAdd}
+       onRemoveImage={handleRemove}
        />
 
       </Screen>
--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import ImageInput from './ImageInput';
 
-function ImageInputList(props) {
+function ImageInputList({imageUris=[],onRemoveImage,onAddImage}) {
 return (
-<View style={styles.container}></View>
+<View style={styles.container}>
+  {imageUris.map((uri)=>
+  <View style={styles.image} key={uri}>
+  <ImageInput imageUri={uri}  onChangeImage={()=>onRemoveImage(uri)}/>
+  </View>
+  )}
+
+  <ImageInput onChangeImage={(uri) => onAddImage(uri)}/>
+
+</View>
 );
 }
 
@@ -11,7 +21,12 @@ const styles = StyleSheet.create({
 container:
 {
   flexDirection:'row',
+},
+image:
+{
+  marginRight:15,
 }
+
 
 })
 
