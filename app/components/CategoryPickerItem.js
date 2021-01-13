7d82fcaeeb933d7ee19bb86a04b8a81a209596c5
Noaman <caedriel@live.com>
2021-01-13 23:44:36 +0400

    fixed spacing issue with catergorypickeritem

--- app/components/CategoryPickerItem.js
+++ app/components/CategoryPickerItem.js
@@ -1,31 +1,28 @@
-import React from 'react';
-import { View,StyleSheet } from 'react-native';
-import AppText from './AppText';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import AppText from "./AppText";
 import Icon from "./icon";
 
-function CategoryPickerItem({item,onPress}) {
-    return (
-        <View style={styles.container}>
-            <Icon backgroundColor={item.backgroundColor} name={item.icon} size={80}/>
-            <AppText style={styles.label}>{item.label}</AppText>
-        </View>
-    );
+function CategoryPickerItem({ item, onPress }) {
+  return (
+    <View style={styles.container}>
+      <Icon backgroundColor={item.backgroundColor} name={item.icon} size={80} />
+      <AppText style={styles.label}>{item.label}</AppText>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container :
-    {
-        padding:30,
-        marginVertical:20,
-        alignItems:"center",
-
-    },
-    label :
-    {
-        marginTop:5,
-        textAlign:"center",
-
-    },
-})
+  container: {
+    padding: 30,
+    marginVertical: 20,
+    alignItems: "center",
+    width: "33%",
+  },
+  label: {
+    marginTop: 5,
+    textAlign: "center",
+  },
+});
 
 export default CategoryPickerItem;
