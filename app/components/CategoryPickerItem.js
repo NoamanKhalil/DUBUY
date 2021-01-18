53d76865b39490ad1b2fffe0720d12d20f8809bb
Noaman <caedriel@live.com>
2021-01-18 20:12:21 +0400

    fixed button bug when selecting item catergory. gps might be broken

--- app/components/CategoryPickerItem.js
+++ app/components/CategoryPickerItem.js
@@ -1,13 +1,19 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
 import AppText from "./AppText";
 import Icon from "./icon";
 
 function CategoryPickerItem({ item, onPress }) {
   return (
     <View style={styles.container}>
-      <Icon backgroundColor={item.backgroundColor} name={item.icon} size={80} />
-      <AppText style={styles.label}>{item.label}</AppText>
+      <TouchableOpacity onPress={onPress}>
+        <Icon
+          backgroundColor={item.backgroundColor}
+          name={item.icon}
+          size={80}
+        />
+        <AppText style={styles.label}>{item.label}</AppText>
+      </TouchableOpacity>
     </View>
   );
 }
--- app/components/Forms/AppFormPicker.js
+++ app/components/Forms/AppFormPicker.js
@@ -1,25 +1,31 @@
-import { useFormikContext } from 'formik';
-import React from 'react';
-import AppPicker from '../AppPicker';
-import ErrorMessage from './ErrorMessage';
+import { useFormikContext } from "formik";
+import React from "react";
+import AppPicker from "../AppPicker";
+import ErrorMessage from "./ErrorMessage";
 
-
-function AppFormPicker({items,numberOfColumns,name,placeholder,width,PickerItemComponent}) {
-    const {errors,setFieldValue,touched,values}= useFormikContext();
-    return (
-        <>
-        <AppPicker
-         items={items}
-         numberOfColumns={numberOfColumns}
-         onSelectedItem={(item)=> setFieldValue(name,item)}
-         PickerItemComponent={PickerItemComponent}
-         placeholder={placeholder}
-         selectedItem={values[name]}
-         width={width}
-        />
-        <ErrorMessage error={errors[name]} visible={touched[name]}/>
-        </>
-    );
+function AppFormPicker({
+  items,
+  name,
+  numberOfColumns,
+  PickerItemComponent,
+  placeholder,
+  width,
+}) {
+  const { errors, setFieldValue, touched, values } = useFormikContext();
+  return (
+    <>
+      <AppPicker
+        items={items}
+        numberOfColumns={numberOfColumns}
+        onSelectedItem={(item) => setFieldValue(name, item)}
+        PickerItemComponent={PickerItemComponent}
+        placeholder={placeholder}
+        selectedItem={values[name]}
+        width={width}
+      />
+      <ErrorMessage error={errors[name]} visible={touched[name]} />
+    </>
+  );
 }
 
 export default AppFormPicker;
