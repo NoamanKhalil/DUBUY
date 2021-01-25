35f34d79aa78c45b9d17c295cdf22059630fb7c6
Noaman <caedriel@live.com>
2021-01-25 18:49:18 +0400

    built app navigator

--- App.js
+++ App.js
@@ -33,11 +33,13 @@ import * as Permissions from "expo-permissions";
 import ImageInputList from "./app/components/ImageInputList";
 import { NavigationContainer } from "@react-navigation/native";
 import AuthNavigator from "./app/navigation/AuthNavigator";
+import navigationTheme from "./app/navigation/navigationTheme";
+import AppNavigator from "./app/navigation/AppNavigator";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <AuthNavigator />
+    <NavigationContainer theme={navigationTheme}>
+      <AppNavigator />
     </NavigationContainer>
   );
 }
--- app/navigation/AuthNavigator.js
+++ app/navigation/AuthNavigator.js
@@ -15,8 +15,16 @@ const AuthNavigator = () => (
       component={WelcomeScreen}
       options={{ headerShown: false }}
     />
-    <Stack.Screen name="Login" component={LoginScreen} />
-    <Stack.Screen name="Register" component={RegisterScreen} />
+    <Stack.Screen
+      name="Login"
+      component={LoginScreen}
+      options={{ headerShown: false }}
+    />
+    <Stack.Screen
+      name="Register"
+      component={RegisterScreen}
+      options={{ headerShown: false }}
+    />
   </Stack.Navigator>
 );
 
