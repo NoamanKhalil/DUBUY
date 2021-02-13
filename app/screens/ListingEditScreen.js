e25239b4b18ac64e1a5838ecde938e00b1bd961a
Noaman <caedriel@live.com>
2021-02-13 19:04:28 +0400

    changes to validation schema messages

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -18,10 +18,10 @@ import { useState } from "react";
 import UploadScreen from "./UploadScreen";
 
 const validationSchema = Yup.object().shape({
-  title: Yup.string().required().min(1).label("Title"),
-  price: Yup.number().required().min(1).max(10000).label("Price"),
+  title: Yup.string().required("Required").min(1).label("Title"),
+  price: Yup.number().required("Required").min(1).max(10000).label("Price"),
   description: Yup.string().label("Description"),
-  category: Yup.object().required().nullable().label("Category"),
+  category: Yup.object().required("Required").nullable().label("Category"),
   images: Yup.array().min(1, "Please select at least one image."),
 });
 
